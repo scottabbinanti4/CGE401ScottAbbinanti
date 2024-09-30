6bd4e6c32b372cae539582884ab47229e6bd2667
scottabbinanti4 <scottabbinanti1@gmail.com>
2024-09-29 19:56:49 -0500

    Challenge 3 Finish

---
+++ CGE401Assignments/Assets/Challenge 3/Scripts/PlayerControllerX3.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Scott Abbinanti
+ * PlayerControllerX3
+ * CGE 401 Challenge 3
+ * Script to control player
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX3 : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip boing;
+
+    bool isLowEnough;
+
+    public int score = 0;
+
+    public int scoreToWin = 10;
+
+    public Text textbox;
+
+    public GameObject gameOverText;
+    public GameObject winText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        playerRb = GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+        isLowEnough = true;
+
+        textbox.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textbox.text = "Score: " + score;
+
+        if (transform.position.y < 13)
+        {
+            isLowEnough = true;
+        }
+        else
+        {
+            isLowEnough = false;
+        }
+
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        if (gameOver && score < scoreToWin)
+        {
+            gameOverText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if(score >= scoreToWin)
+        {
+            gameOver = true;
+            winText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+            score++;
+        }
+
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            playerAudio.PlayOneShot(boing, 1.0f);
+            //Destroy(gameObject);
+        }
+    }
+
+}
