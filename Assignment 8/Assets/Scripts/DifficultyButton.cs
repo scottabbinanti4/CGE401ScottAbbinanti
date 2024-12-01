3469a2637536a53d908fb27e22099ea7b29af9ac
scottabbinanti4 <scottabbinanti1@gmail.com>
2024-12-01 16:42:36 -0600

    Prototype5 Finish

---
+++ Assignment 8/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Scott Abbinanti 
+ * DifficultyButton
+ * Assignment 8
+ * Controls difficulty buttons on menu
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+
+    private GameManager gm;
+
+    public float difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDifficulty()
+    {
+        gm.StartGame(difficulty);
+    }
+}
---
+++ Assignment 8/Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Scott Abbinanti 
+ * GameManager
+ * Assignment 8
+ * Manages the game
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive;
+
+    public Button restart;
+
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame(float difficulty)
+    {
+        menu.SetActive(false);
+
+        spawnRate /= difficulty;
+
+        score = 0;
+        isGameActive = true;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restart.gameObject.SetActive(true);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
