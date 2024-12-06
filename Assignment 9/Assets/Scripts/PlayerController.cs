2c0fa5ae97ac7e3aa135b23dde9dfc50d252af4a
scottabbinanti4 <scottabbinanti1@gmail.com>
2024-12-06 14:37:57 -0600

    1:50:36

---
+++ Assignment 9/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿/* 
+ * Scott Abbinanti
+ * PlayerController
+ * Assignment 9
+ * Makes character move where player clicks
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<ThirdPersonCharacter>();
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
